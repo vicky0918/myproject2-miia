f0a36c598590fec6db862862642257cafafe5ed4
User <vickywan90@gmail.com>
2022-03-11 15:23:43 +0800

    no debug

--- src/main/java/com/vicky/Scoring.java
+++ src/main/java/com/vicky/Scoring.java
@@ -2,9 +2,9 @@ package com.vicky;
 
 public class Scoring {
     public static void main(String[] args) {
-        Student vicky = new Student();
-        vicky.name = "Vicky";
-        vicky.english = 70;
-        vicky.math = 90;
+        Student vicky = new Student("Vicky",70,90);
+        Student hank = new Student("Hank",60,88);
+        vicky.print();
+        hank.print();
     }
 }
--- src/main/java/com/vicky/Student.java
+++ src/main/java/com/vicky/Student.java
@@ -4,4 +4,29 @@ public class Student {
     String name;
     int english;
     int math;
+    public  Student(String name){
+        this.name = name;
+    }
+    public Student(String name,int english,int math){
+        this(name);
+        //this.name = name;
+        this.math = math;
+        this.english = english;
+    }
+    public Student(){
+        this("Jone Doe",-1,-1);
+        /*name = "Jone Doe";
+        english = -1;
+        math = -1;*/
+    }
+
+    public void print(){
+        int average = (english + math)/2;
+        System.out.print(name + "\t"
+                + math + "\t" + english+ average);
+        if(average < 60){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 }
