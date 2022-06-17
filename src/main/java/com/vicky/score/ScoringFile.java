d614ba63366c3bd75d771caca5b515eae2fe1ebd
vicky0918 <93139591+vicky0918@users.noreply.github.com>
2022-06-17 16:28:58 +0800

    6/17 class

--- src/main/java/com/vicky/score/ScoringFile.java
+++ src/main/java/com/vicky/score/ScoringFile.java
@@ -1,5 +1,6 @@
 package com.vicky.score;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,13 +9,22 @@ public class ScoringFile {
     public static void main(String[] args) {
         try{
             FileReader fileReader = new FileReader("scores.txt");
-            int data = fileReader.read();
-            System.out.println(data);
-            data = fileReader.read();
-            System.out.println(data);
+            BufferedReader reader = new BufferedReader(fileReader);
+              //Jack,90,45
+            String line = reader.readLine();
+            while (line != null){
+                //System.out.println(line);
+                String[] token =  line.split(",");
+                String name = token[0];
+                int english = Integer.parseInt(token[1]);
+                int math = Integer.parseInt(token[2]);
+                Student stu = new Student(name,english,math);
+                stu.print();
+                line = reader.readLine();
+            }
         }catch (FileNotFoundException e){
             System.out.println("檔案讀取失敗");
-        }catch (IOException e){
+        } catch (IOException e){
             System.out.println("資料讀取失敗");
         }
         System.out.println("Testing");
